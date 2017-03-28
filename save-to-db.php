2bec5be150d9acc027e330b0bd23a1ef45e3c7ef
Jerry <jerry.malmstrom@borealisgroup.com>
2017-03-28 11:13:23 +0200

    Customers.php

--- save-to-db.php
+++ save-to-db.php
@@ -28,7 +28,23 @@
 			],[
 				"username" => $login_session
 			]);
-			
+			break;
+		case "customer_update":
+			$db->update("Customers",[
+				"name" => $_POST["name"]
+			],[
+				"ID" => $_POST["ID"]
+			]);
+			break;
+		case "customer_add":
+			$db->insert("Customers",[
+			"username" => $_POST["username"],
+			"name" => $_POST["name"],
+			"role" => "User",
+			"email" => $_POST["email"],
+			"password" => $_POST["password"],
+			"company" => $_POST["company"],
+			"color" => $_POST["color"]]);
 			break;
 			
 		default:

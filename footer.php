8dafcba4745cf378e96c9772ab2bbcc23af9e288
Jerry <jerry.malmstrom@borealisgroup.com>
2017-04-03 13:04:46 +0200

    -

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="ui section divider">
  </div>
 
-<div class="ui four column centered grid">
+<div class="ui two column centered center aligned grid">
 	<div class="column">1</div>
 	<div class="column">2</div>
 </div>

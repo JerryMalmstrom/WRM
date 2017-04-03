b4c5ca59ab3f9b8ea7c558a5f02c1a348cbe7bff
Jerry <jerry.malmstrom@borealisgroup.com>
2017-04-03 16:04:07 +0200

    asda

--- db-test/web-file-browser.php
+++ db-test/web-file-browser.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__.'/../lib/Dropbox/strict.php';
+require_once __DIR__.'/lib/Dropbox/strict.php';
 
 $appInfoFile = __DIR__."/web-file-browser.app";
 
 // NOTE: You should be using Composer's global autoloader.  But just so these examples
 // work for people who don't have Composer, we'll use the library's "autoload.php".
-require_once __DIR__.'/../lib/Dropbox/autoload.php';
+require_once __DIR__.'/lib/Dropbox/autoload.php';
 
 use \Dropbox as dbx;
 

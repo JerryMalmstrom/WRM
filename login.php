febb0a1b943c6b91774eb8a8de868968c3df354d
skinkost <jerry.malmstrom@gmail.com>
2017-04-04 20:33:25 +0200

    error

--- login.php
+++ login.php
@@ -11,8 +11,7 @@
 		$password = md5($_POST['password']);
 		  
 		$sql = sql_read($db, "SELECT ID FROM users WHERE username = '$username' and password = '$password'");
-	}
-		
+			
 		if($sql->num_rows == 1) {
 			session_register("username");
 			$_SESSION['login_user'] = $username;

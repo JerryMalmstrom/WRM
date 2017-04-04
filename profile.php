46c1621a5597f17ab016411a5b20af1f462724de
Jerry <jerry.malmstrom@borealisgroup.com>
2017-04-04 10:04:05 +0200

    Profile

--- profile.php
+++ profile.php
@@ -11,32 +11,36 @@ $( function() {
 
 <div class="ui content">
 	<div class="ui container">
-	  <table id="user" class="ui single line table">
-		<thead>
-		  <tr>
-			<th>Rad</th>
-			<th>Värde</th>
-		  </tr>
-		</thead>
-		<tbody>
-		<?php
-		
-		$sql = $db->query("SELECT * FROM users WHERE username='$login_session'");
-		
-		
-		while ($d = $sql->fetch_assoc())	{
-			echo "<tr><td>ID</td><td>" . $d["ID"] . "</td></tr>";
-			echo "<tr><td>Användarnamn</td><td><input id='username' type='text' value='" . $d["username"] . "'></td></tr>";
-			echo "<tr><td>Namn</td><td><input id='name' type='text' value='" . $d["name"] . "'></td></tr>";
-			echo "<tr><td>Email</td><td><input id='email' type='text' value='" . $d["email"] . "'></td></tr>";
-			echo "<tr><td>Företag</td><td><input id='company' type='text' value='" . $d["company"] . "'></td></tr>";
-			echo "<tr><td>Roll</td><td>" . $d["role"] . "</td></tr>";
-			echo "<tr><td>Färg</td><td><input type='color' id='color' value='" . trim($d["color"]) . "'></td></tr>";
-			echo "<tr><td>Bild</td><td>" . $d["profileImage"] . "</td></tr>";
-		}
-		?>
-		</tbody>
-	</table>
+		<form>
+			<div class="ui form">
+				<table id="user" class="ui single line table">
+					<thead>
+					  <tr>
+						<th>Rad</th>
+						<th>Värde</th>
+					  </tr>
+					</thead>
+					<tbody>
+					<?php
+					
+					$sql = $db->query("SELECT * FROM users WHERE username='$login_session'");
+					
+					
+					while ($d = $sql->fetch_assoc())	{
+						echo "<tr><td>ID</td><td>" . $d["ID"] . "</td></tr>";
+						echo "<tr><td>Användarnamn</td><td><input id='username' type='text' value='" . $d["username"] . "'></td></tr>";
+						echo "<tr><td>Namn</td><td><input id='name' type='text' value='" . $d["name"] . "'></td></tr>";
+						echo "<tr><td>Email</td><td><input id='email' type='text' value='" . $d["email"] . "'></td></tr>";
+						echo "<tr><td>Företag</td><td><input id='company' type='text' value='" . $d["company"] . "'></td></tr>";
+						echo "<tr><td>Roll</td><td>" . $d["role"] . "</td></tr>";
+						echo "<tr><td>Färg</td><td><input type='color' id='color' value='" . trim($d["color"]) . "'></td></tr>";
+						echo "<tr><td>Bild</td><td>" . $d["profileImage"] . "</td></tr>";
+					}
+					?>
+					</tbody>
+				</table>
+			</div>
+		</form>
 	<button class="ui button" id="update-user">Uppdatera användare</button>
 	</div>
 </div>
